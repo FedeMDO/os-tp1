83c287e3cecb652c515ec2d0c4d7d49a4f4a2b34
Federico Montes de Oca <federicomdo97@gmail.com>
2019-09-24 14:10:23 -0300

    finishing exercice3

--- exercice3/Exercice3.c
+++ exercice3/Exercice3.c
@@ -6,17 +6,118 @@
 #include "dlist.h" 
 #include "list.h"
 
+List init_list(List li, int from, int to){
+    if(from < to){
+        for (int i = from; i <= to; i++){
+            li = push_back_list(li, i);
+        }
+        printf("La liste\n");
+        print_list(li);
+    }
+    return li;
+}
+
+List concatLists(List first, List second){
+    first->end->next = second->begin;
+    first->end = second->end;
+    return first;
+}
+
+DList fromListToDlist(List li, DList dli){
+    ListNode *temp = li->begin;
+
+    while (temp != NULL)
+    {
+        dli = push_back_dlist(dli, temp->value);
+        temp = temp->next;
+    }
+    print_dlist(dli);
+    return dli;
+}
+
+DList fromDListToCircular(DList dli){
+    int m = 20;
+    for (int i = 15; i <= m; i++){
+        dli = push_back_dlist(dli, i);
+    }
+    printf("    La liste: \n    ");
+    print_dlist(dli);
+
+    // Circulaire (the first node has a reference to the last node of the list)
+    dli->begin->back = dli->end;
+    dli->end->next = dli->begin;
+    return dli;
+}
+
+DList init_dlist(DList dli, int from, int to){
+    for (int i = from; i <= to ; i++){
+        dli = push_back_dlist(dli, i);
+    }
+    printf("    La liste: \n    ");
+    print_dlist(dli);
+    return dli;
+}
+
+void descendant_order_list(DList dli, int n){
+    DListNode *temp2Desc= dli->end; // begin from the end
+
+    // if n > list length then we cant work
+    // we do n = length and we say it to the client ok 
+    if(n > dli->length){
+        printf("Sorry, the max length of the given list is %d. Changing value of n from %d to %d \n", 
+                dli->length, n, dli->length);
+        n = dli->length;
+    } else if (n < 1) {
+        return;
+    }
+    printf("    n final value is %d, we will iterate %d times\n", n, n);   
+
+    // start the descending loop n iterations.
+    int j = 0;
+    printf("    ");   
+    while (j < n)
+    {
+        printf("[%d] ", temp2Desc->value);
+        temp2Desc = temp2Desc->back;
+        j++;
+    }
+    printf("\n");   
+    return;
+}
+
+void showingCircularBehavior(DList dli){
+    printf("    Demonstration (boucle 10 fois harcoded): \n");
+    DListNode *dTemp = dli->begin;
+    int i = 0;
+    printf("    ");
+    while (i < 10)
+    {
+        printf("%d ", dTemp->value);
+        dTemp = dTemp->next;
+        i++;
+    }
+    printf("\n");
+}
+
+void square_list(List li){
+    printf("RESULT: \n");    
+    ListNode *temp2Square = li->begin;
+
+    while (temp2Square != NULL)
+    {
+        printf("[%f] ",((double) temp2Square->value * (double) temp2Square->value));
+        temp2Square = temp2Square->next;
+    }
+    printf("\n");   
+    return; 
+}
+
 int main(int argc, char const *argv[])
 {
     // 1 Créez une liste chaînée contenant les n premiers entiers dans l’ordre croissant
     printf("\n1 - Créez une liste chaînée contenant les n premiers entiers dans l’ordre croissant \n");
     List list1 = new_list();
-    int i, n = 10;
-    for (i = 1; i <= n; i++){
-        list1 = push_back_list(list1, i);
-    }
-    printf("La liste\n");
-    print_list(list1);
+    list1 = init_list(list1, 1, 10);
 
     // 2 Créez une fonction qui renvoie la longueur d’une liste.
     printf("\n2 - Créez une fonction qui renvoie la longueur d’une liste \n");
@@ -28,7 +129,7 @@ int main(int argc, char const *argv[])
 
     // 4 Écrivez une fonction qui retourne la liste des carrés d’une autre liste passée en para-mètre.
     printf("\n4 - Écrivez une fonction qui retourne la liste des carrés d’une autre liste \n");
-    printf("TODO\n");// TODO    
+    square_list(list1);
 
     // 5 Retirez le premier élément d’une liste.
     printf("\n5 - Retirez le premier élément \n");
@@ -52,52 +153,36 @@ int main(int argc, char const *argv[])
 
     // 9 Écrire une fonction qui concatène deux listes
     printf("\n9 - Écrire une fonction qui concatène deux listes \n");
-    printf("TODO\n");// TODO    
-    
+    List list2 = new_list();
+    printf("creating list2 (that is going to be concatened with list1) \n");
+    printf("we want list2 first, so we put list2 as the first argument \n");
+
+    list2 = init_list(list2, 100, 110);
+    list2 = concatLists(list2, list1);
+    printf("RESULT: \n");
+    print_list(list2);
+
     // 10 Transformez votre liste en une liste doublement chaînée
     printf("\n10 - Transformez votre liste en une liste doublement chaînée \n");
     DList dlist1 = new_dlist();
-    ListNode *temp = list1->begin;
-
-    while (temp != NULL)
-    {
-        dlist1 = push_back_dlist(dlist1, temp->value);
-        temp = temp->next;
-    }
-    print_dlist(dlist1);
+    dlist1 = fromListToDlist(list1, dlist1);
 
     // 11 Créez une liste doublement chaînée circulaire (adaptez les fonctions précédentes)
     printf("\n11 - Créez une liste doublement chaînée circulaire \n");
-
+    
     printf("    Initialization du Liste doublement chainée \n");
     DList dlist2 = new_dlist();
-    dlist2 = push_back_dlist(dlist2, 1);
-    dlist2 = push_back_dlist(dlist2, 2);
-    dlist2 = push_back_dlist(dlist2, 3);
-    printf("    La liste: \n    ");
-    print_dlist(dlist2);
-
-    // Circulaire (the first node has a reference to the last node of the list)
-    dlist2->begin->back = dlist2->end;
-    dlist2->end->next = dlist2->begin;
+    dlist2 = fromDListToCircular(dlist2);
     printf("    Comportement circulaire prêt \n");
 
     // Demontration (10 fois)
-    printf("    Demonstration (boucle 10 fois harcoded): \n");
-    DListNode *dTemp = dlist2->begin;
-    i = 0;
-    printf("    ");
-    while (i < 10)
-    {
-        printf("%d ", dTemp->value);
-        dTemp = dTemp->next;
-        i++;
-    }
-    printf("\n");
+    showingCircularBehavior(dlist2);
 
     // 12 Afficher les les n premiers entiers dans l’ordre décroissant.
     printf("\n12 - Afficher les les n premiers entiers dans l’ordre décroissant \n");
-    printf("TODO\n");// TODO    
+    DList dlist3 = new_dlist();
+    dlist3 = init_dlist(dlist3, 80, 100);
+    descendant_order_list(dlist3, 12); 
 
     return 0;
 }
